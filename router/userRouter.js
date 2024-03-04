1d53b82e71679dbb485a661ddfe1ae46a62d19f2
tiw_13 <samarn.sae@gmail.com>
2024-03-04 20:04:47 +0700

    dsfdsf

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,8 +2,16 @@ import express from "express";
 import { list, get, put } from "../controller/userDBController.js";
 import verifyRoles from "../middleware/verifyRoles.js";
 import ROLES_LIST from "../config/rolesList.js";
-let router = express.Router();
-router.get("/", verifyRoles(ROLES_LIST.Admin), list);
-router.get("/:username", verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Nurse), get);
-router.put("/:username", verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Nurse), put);
-export default router;
+let userRouter = express.Router();
+userRouter.get("/", verifyRoles(ROLES_LIST.Admin), list);
+userRouter.get(
+  "/:username",
+  verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Nurse),
+  get
+);
+userRouter.put(
+  "/:username",
+  verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Nurse),
+  put
+);
+export default userRouter;
