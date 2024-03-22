1145d5a6681892a434b9c5048ad6fc5817cbb2ed
tiw_13 <samarn.sae@gmail.com>
2024-03-22 14:57:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
-    // origin: ["https://tu-wellness-center.vercel.app"],
+    // origin: ["http://localhost:3000"],
+    origin: ["https://tu-wellness-center.vercel.app"],
   })
 );
 
